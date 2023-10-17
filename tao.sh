1d81da47122af9029d38c2e4fd2852d9addc5ed0
青春不留白 <115192496+taotao1058@users.noreply.github.com>
2023-10-17 19:19:08 +0800

    Update tao.sh

--- tao.sh
+++ tao.sh
@@ -1,63 +1,63 @@
 #!/bin/bash
 
-# 安装 FFmpeg
+# 检查是否已经安装了 yum
+if ! command -v yum &> /dev/null
+then
+    echo "yum 未安装，退出..."
+    exit 1
+fi
+
+# 检查是否已经安装了 FFmpeg
 if ! command -v ffmpeg &> /dev/null
 then
-    echo "FFmpeg could not be found, installing now..."
-    sudo apt update
-    sudo apt install ffmpeg
+    echo "FFmpeg 未安装，开始安装..."
+    yum install -y epel-release
+    rpm -v --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro
+    rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
+    yum install -y ffmpeg ffmpeg-devel
+else
+    echo "FFmpeg 已安装"
 fi
 
-# 安装 dos2unix
-if ! command -v dos2unix &> /dev/null
-then
-    echo "dos2unix could not be found, installing now..."
-    sudo apt install dos2unix
-fi
+stream_start(){
+    # 定义推流地址和推流码
+    read -p "输入你的推流地址和推流码(rtmp协议):" rtmp
 
-# 创建视频文件夹
-folder_path="/home/ubuntu/so"
-if [ ! -d "$folder_path" ]
-then
-    echo "Creating video folder..."
-    mkdir "$folder_path"
-fi
+    # 判断用户输入的地址是否合法
+    if [[ $rtmp =~ "rtmp://" ]];then
+        echo "推流地址输入正确,程序将进行下一步操作."
+        sleep 2
+    else  
+        echo "你输入的地址不合法,请重新运行程序并输入!"
+        exit 1
+    fi 
 
-# 创建推流脚本
-script_path="/home/ubuntu/you.sh"
-cat > "$script_path" << EOL
-#!/bin/bash
+    # 定义视频存放目录
+    read -p "输入你的视频存放目录 (格式仅支持mp4,并且要绝对路径,例如/opt/video):" folder
 
-# 设置视频文件夹路径
-folder_path="/home/ubuntu/so"
-
-# 输入推流服务器地址
-echo "请输入你的推流服务器地址（例如：rtmp://bdy.live-push.bilivideo.com/live-bvc）："
-read server_address
-
-# 输入串流密钥
-echo "请输入你的串流密钥（例如：?streamname=live_4756957_8900165&key=c86fb10b3b83d6176125de5bea4&schedule=rtmp&pflag=1）："
-read stream_key
-
-# 开始推流
-while true
-do
-    for file in "\$folder_path"/*
+    # 循环
+    while true
     do
-        if [[ \$file == *.mp4 ]] || [[ \$file == *.avi ]] || [[ \$file == *.mkv ]] || [[ \$file == *.flv ]] || [[ \$file == *.mov ]]
-        then
-            ffmpeg -re -i "\$file" -vcodec copy -acodec aac -b:a 192k -f flv "\$server_address/\$stream_key"
-            sleep 2
-        fi
+        cd $folder
+        for video in $(ls *.mp4)
+        do
+            ffmpeg -re -i "$video" -c:v copy -c:a aac -b:a 192k -strict -2 -f flv ${rtmp}
+        done
     done
-done
-EOL
+}
 
-# 转换脚本格式
-dos2unix "$script_path"
-
-# 为脚本赋予可执行权限
-chmod +x "$script_path"
-
-# 开启后台运行
-nohup "$script_path" &
+# 开始菜单设置
+echo "CentOS7 X86_64 FFmpeg无人值守循环推流"
+echo "1.开始无人值守循环推流"
+start_menu(){
+    read -p "请输入数字(1),选择你要进行的操作:" num
+    case "$num" in
+        1)
+        stream_start
+        ;;
+        *)
+        echo "请输入正确的数字 (1)"
+        ;;
+    esac
+}
+start_menu
