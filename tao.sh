1f29383294170e602a58974886503438ab8dc312
青春不留白 <115192496+taotao1058@users.noreply.github.com>
2023-10-17 16:22:31 +0800

    Update tao.sh

--- tao.sh
+++ tao.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
-# 手动输入视频文件路径和Bilibili直播流密钥
-read -p "请输入视频文件路径：" VIDEO_FILE
-read -p "请输入Bilibili直播流密钥：" STREAM_KEY
+# 检查FFmpeg是否已经安装
+if ! command -v ffmpeg &> /dev/null
+then
+    echo "FFmpeg could not be found, installing now..."
+    sudo apt update
+    sudo apt install ffmpeg
+fi
 
-# 下载最新版FFmpeg
-echo "正在下载最新版FFmpeg..."
-wget https://ffmpeg.org/releases/ffmpeg-latest.tar.gz
-tar -xf ffmpeg-latest.tar.gz
-cd ffmpeg-*
-./configure
-make
-sudo make install
+# 设置视频文件夹路径
+folder_path="/home/ubuntu/so"
 
-# 循环不间断推流直播
-while true; do
-  # 使用FFmpeg推流到B站
-  ffmpeg -re -i "$VIDEO_FILE" -c:v copy -c:a aac -f flv "rtmp://live-push.bilivideo.com/live-bvc/$STREAM_KEY" </dev/null >/dev/null 2>&1 &
+# 输入推流服务器地址
+echo "请输入你的推流服务器地址（例如：rtmp://bdy.live-push.bilivideo.com/live-bvc）："
+read server_address
 
-  # 获取FFmpeg进程ID
-  ffmpeg_pid=$!
+# 输入串流密钥
+echo "请输入你的串流密钥（例如：?streamname=live_475864957_89400165&key=c836fb100b31b83d6174612b5de5bea4&schedule=rtmp&pflag=1）："
+read stream_key
 
-  # 等待FFmpeg进程结束
-  wait $ffmpeg_pid
+# 开始推流
+while true
+do
+    for file in "$folder_path"/*
+    do
+        if [[ $file == *.mp4 ]] || [[ $file == *.avi ]] || [[ $file == *.mkv ]] || [[ $file == *.flv ]] || [[ $file == *.mov ]]
+        then
+            ffmpeg -re -i "$file" -vcodec copy -acodec aac -b:a 192k -f flv "$server_address/$stream_key"
+            sleep 2
+        fi
+    done
 done
