2b5fb9136f17f9d44f60d4e3df47c27bca310cf3
青春不留白 <115192496+taotao1058@users.noreply.github.com>
2023-10-20 00:00:47 +0800

    Update tao.sh

--- tao.sh
+++ tao.sh
@@ -1,55 +1,80 @@
 #!/bin/bash
 
-# 检查FFmpeg是否已经安装
-if ! command -v ffmpeg &> /dev/null
-then
-    echo "FFmpeg could not be found, installing now..."
-    sudo apt update
-    sudo apt install ffmpeg
-fi
+ffmpeg_install() {
+    # 检查FFmpeg是否已经安装
+    if ! command -v ffmpeg &> /dev/null; then
+        echo "FFmpeg could not be found, installing now..."
+        sudo apt update
+        sudo apt install ffmpeg
+    fi
+}
 
-# 提示用户输入视频文件路径
-echo "请输入视频文件夹的完整路径（例如：/home/video）："
-read folder_path
+stream_start() {
+    # 提示用户输入视频文件路径
+    read -p "请输入视频文件夹的完整路径（例如：/home/video）：" folder_path
 
-# 检查输入的路径是否存在
-if [ ! -d "$folder_path" ]
-then
-    echo "输入的路径不存在，请重新输入。"
-    exit 1
-fi
+    # 检查输入的路径是否存在
+    if [ ! -d "$folder_path" ]; then
+        echo "输入的路径不存在，请重新输入。"
+        exit 1
+    fi
 
-# 输入推流服务器地址
-echo "请输入你的推流服务器地址："
-read server_address
+    # 输入推流服务器地址
+    read -p "请输入你的推流服务器地址：" server_address
 
-# 输入串流密钥
-echo "请输入你的串流密钥："
-read stream_key
+    # 输入串流密钥
+    read -p "请输入你的串流密钥：" stream_key
 
-# 提示用户输入他们想要的视频码率
-echo "请输入你希望的视频码率（例如：2000k）："
-read video_bitrate
+    # 提示用户输入他们想要的视频码率
+    read -p "请输入你希望的视频码率（例如：2000k）：" video_bitrate
 
-# 提示用户输入他们想要的帧率
-echo "请输入你希望的帧率（例如：30）："
-read frame_rate
+    # 提示用户输入他们想要的帧率
+    read -p "请输入你希望的帧率（例如：30）：" frame_rate
 
-# 开始推流
-echo "开始推流..."
-while true
-do
-    for file in "$folder_path"/*
-    do
-        if [ $file == *.mp4 ]
-        then
+    # 开始推流
+    echo "开始推流..."
+    while true; do
+        for file in "$folder_path"/*.mp4; do
             echo "开始转码文件：$file"
             # 转码以应用新的码率和帧率
-            ffmpeg -re -i "$file" -vcodec libx264 -acodec aac -b:a 128k -b:v $video_bitrate -r $frame_rate -f flv "$server_address/$stream_key"
+            ffmpeg -re -i "$file" -vcodec libx264 -acodec aac -b:a 128k -b:v "$video_bitrate" -r "$frame_rate" -f flv "$server_address/$stream_key"
             echo "转码完成：$file"
-        fi
+        done
+
+        # 添加延迟，避免过快无限循环
+        sleep 1
     done
-    
-    # 添加延迟，避免过快无限循环
-    sleep 1
-done
+}
+
+stream_stop() {
+    # 停止推流
+    echo "停止推流。"
+    pkill ffmpeg
+}
+
+# 开始菜单设置
+echo "一键运行 FFmpeg 推流脚本"
+echo "1. 安装 FFmpeg"
+echo "2. 开始推流"
+echo "3. 停止推流"
+
+start_menu() {
+    read -p "请输入数字(1-3)，选择你要进行的操作：" num
+    case "$num" in
+        1)
+            ffmpeg_install
+            ;;
+        2)
+            stream_start
+            ;;
+        3)
+            stream_stop
+            ;;
+        *)
+            echo "请输入正确的数字 (1-3)"
+            ;;
+    esac
+}
+
+# 运行开始菜单
+start_menu
